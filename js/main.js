6369398eb13706d1f059fc09eb1cf0f8b4a70caa
max <remaxpet@gmail.com>
2016-10-01 14:57:39 +0300

    get rid of some redundant functions

--- js/main.js
+++ js/main.js
@@ -11,28 +11,6 @@ function drawChildren(index) {
 var index = getRandom(10);
 drawChildren(index);
 
-function createCookie(name, value, days) {
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime() + days*24*60*60*1000);
-		var expires = "; expires=" + date.toGMTString();
-	} else {
-		var expires = "";
-	}
-	document.cookie = name + "=" + value + expires + "; path=/";
-}
-
-function readCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for (var i = 0; i < ca.length; i++) {
-		var c = ca[i];
-		c = c.trim();
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-	}
-	return null;
-}
-
 var btnBad = document.getElementById("bad");
 var btnGood = document.getElementById("good");
 
